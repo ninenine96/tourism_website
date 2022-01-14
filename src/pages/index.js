350e17c8ddeb66130ef69a3d80da9a13f49fed77
Mohammad R <mohammadruhaan@gmail.com>
2022-01-15 00:25:10 +0530

    bruh

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ImageSlider from '../components/ImageSlider'
-import Packages from '../components/Packages'
+
 import { SliderData } from '../components/SliderData'
 
 const Home = () => {
