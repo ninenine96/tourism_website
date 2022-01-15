21e315b4bc94368e1cc90023f6659668cb7b9682
Mohammad R <mohammadruhaan@gmail.com>
2022-01-16 00:21:35 +0530

    layout and footer changes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,14 +1,36 @@
 import React from 'react'
+import { Button } from '../../globalStles'
 import {
     FooterContainer,
     FooterSubscription,
     FooterSubHeading,
     FooterSubText,
     Form,
-    FormInput
+    FormInput,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinksItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    SocialIcon,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink
 
 } from './styles/footer'
-import { Button } from '../../globalStles'
+
+import {
+    FaFacebook,
+    FaInstagram,
+    FaYoutube,
+    FaTwitter,
+    FaLinkedin
+} from 'react-icons/fa';
+
+
 
 const Footer = () => {
     return (
@@ -24,6 +46,66 @@ const Footer = () => {
                     <Button fontBig>Yes</Button>
                 </Form>
             </FooterSubscription>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>About Us</FooterLinkTitle>
+                        <FooterLink to="/sign-up">Sign Up</FooterLink>
+                        <FooterLink to="/packages">Packages</FooterLink>
+                        <FooterLink to="/contact us">Contact Us</FooterLink>
+                        <FooterLink to="/about">About</FooterLink>
+                    </FooterLinksItems>
+                    
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Videos</FooterLinkTitle>
+                        <FooterLink to="/">YouTube Testimonials</FooterLink>
+                        <FooterLink to="/">Kashmir</FooterLink>
+                    </FooterLinksItems>
+                    
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Social Media</FooterLinkTitle>
+                        <FooterLink to="/">Facebook</FooterLink>
+                        <FooterLink to="/">Instagram</FooterLink>
+                        <FooterLink to="/">WhatsApp</FooterLink>
+
+                    </FooterLinksItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
+            <SocialMedia>
+                <SocialMediaWrap>
+                    <SocialLogo to='/'>
+                    <SocialIcon />
+                    </SocialLogo>
+                    <WebsiteRights>M.R. Sofi © 2022</WebsiteRights>
+                    <SocialIcons>
+                        <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                        <FaFacebook />
+                        </SocialIconLink>
+                        
+                        <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                        <FaInstagram />
+                        </SocialIconLink>
+                        
+                        <SocialIconLink
+                        href='/'
+                        rel='noopener noreferrer'
+                        target='_blank'
+                        aria-label='Youtube'
+                        >
+                        <FaYoutube />
+                        
+                        </SocialIconLink>
+                        <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+                        <FaTwitter />
+                        </SocialIconLink>
+                        
+                        <SocialIconLink href='/' target='_blank' aria-label='LinkedIn'>
+                        <FaLinkedin />
+                        </SocialIconLink>
+                    </SocialIcons>
+                </SocialMediaWrap>
+            </SocialMedia>
+
         </FooterContainer>
     )
 }
